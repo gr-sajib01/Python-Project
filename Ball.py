48749d780aa49dabb4f66c7415ea1918bd6937e9
Md Golam Rabbani Sajib <grsajib600@gmail.com>
2026-03-11 14:25:00 +0600

    Implement ball movement and drawing methods
    Add movement and drawing functionality for the ball.

--- Ball.py
+++ Ball.py
@@ -9,3 +9,18 @@ class Ball:
             settings.BALL_SIZE,
             settings.BALL_SIZE
         )
+    self .dx= settings.BALL_SPEED
+    self .dy= -settings.BALL_SPEED
+
+def move(self):
+    self.rect.x += self.dx
+    self .rect.y +=self.dy
+
+if self.rect.left <=0 or self.rect.right>=settings.WIDTH:
+    self.dx *= -1
+
+if self.rect.top <=0:
+    self.dy *= -1
+
+def draw(self,screen):
+    pygame.draw.rect(screen,(300,300,300),self.rect)
