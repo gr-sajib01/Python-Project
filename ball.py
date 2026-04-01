be921ff7b0ce985f65c93c2f73b93cf35a420178
Md Golam Rabbani Sajib <grsajib600@gmail.com>
2026-04-02 05:14:04 +0600

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,9 +1,9 @@
 import pygame
 import settings
 
-class ball:
+class Ball:
     def __init__(self):
-        self.rect=pygame.Rect(
+        self.rect = pygame.Rect(
             settings.WIDTH//2,
             settings.HEIGHT//2,
             settings.BALL_SIZE,
@@ -13,15 +13,15 @@ class ball:
         self.dx = settings.BALL_SPEED
         self.dy = -settings.BALL_SPEED
 
-def move(self):
+    def move(self):
         self.rect.x += self.dx
         self.rect.y += self.dy
 
-        if self.rect.left <=0 or self.rect.right>=settings.WIDTH:
+        if self.rect.left <= 0 or self.rect.right >= settings.WIDTH:
             self.dx *= -1
 
-        if self.rect.top <=0:
+        if self.rect.top <= 0:
             self.dy *= -1
 
-def draw(self,screen):
-    pygame.draw.rect(screen,(255,255,255),self.rect)
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255,255,255), self.rect)
